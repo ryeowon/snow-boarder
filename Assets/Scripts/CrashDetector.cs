a0372c344a338262f62c01f25df1613538011406
ryeowon hong <ryeowon13@gmail.com>
2023-04-12 17:57:14 +0900

    Scene 전환

---
+++ Assets/Scripts/CrashDetector.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CrashDetector : MonoBehaviour
+{
+    [SerializeField] float loadDelay = 0.1f;
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.tag == "Ground") {
+            Invoke("ReloadScene", loadDelay);
+        }
+    }
+
+    void ReloadScene() {
+        SceneManager.LoadScene(0); 
+    }
+}
